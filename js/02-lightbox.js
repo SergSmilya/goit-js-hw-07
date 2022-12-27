2117bade77e2548023d4aae9c18d487d80f53cdd
SergSmilya <sergsmilichenko@gmail.com>
2022-12-27 12:18:21 +0200

    final

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const refs = {
+  ulAdd: document.querySelector(".gallery"),
+};
+
+function createElementsGalery(galleryObj) {
+  return galleryItems
+    .map(({ preview, original, description } = 0) => {
+      const element = `<a class="gallery__item" href=${original}>
+  <img class="gallery__image" src=${preview} alt=${description} />
+</a>`;
+      return element;
+    })
+    .join("");
+}
+
+const stringElement = createElementsGalery(galleryItems);
+
+refs.ulAdd.insertAdjacentHTML("afterbegin", stringElement);
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+  captionsData: "alt",
+});
